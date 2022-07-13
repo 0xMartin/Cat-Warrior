cc295a41215cb5a2e855df27576733b365385e76
0xMartin <martin.krcma1@gmail.com>
2022-07-13 13:29:26 +0200

    explosion

--- entity/bullet/bullet_player.gd
+++ entity/bullet/bullet_player.gd
@@ -4,8 +4,8 @@ export var speed = 600
 
 var move = Vector2()
 var right = false
-
 var particles = preload("res://entity/fx/bullet_particles.tscn").instance()
+var explosion = preload("res://entity/fx/explosion.tscn").instance()
 
 func init(_right):
 	right = _right
@@ -13,21 +13,32 @@ func init(_right):
 		speed *= -1
 		$Sprite.flip_h = true
 		
+	# spawn particle efektu
 	particles.init(self, Color.blue, 30, 4, 1)
 	get_parent().add_child(particles)
+
 
 func _physics_process(delta):
 	if not GameConfig.physics_enabled:
 		return	
 	position.x += speed * delta
 
+
 # po uplinuti definovaneho casu odstrani strelu
 func _on_Timer_timeout():
+	hit()
+
+
+# kolize s objektem
+func _on_bullet_player_area_entered(area):
+	hit()
+	
+
+# zasah: destruktor + exploze
+func hit():
+	# exploze
+	get_parent().add_child(explosion)
+	explosion.init(position, Color.blue, 90, 300, 4, 0.2, 0.1)
+	#destruktor
 	particles.kill()
 	queue_free()
-
-
-func _on_bullet_player_area_entered(area):
-	#particles.kill()
-	#queue_free()
-	pass
---
+++ entity/fx/explosion.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# pos - pozice exploze
+# color - barva
+# amout - pocet castic
+# size - velikost castic
+# velocity - rychlost
+# lifetime - delka zivota castic
+# time - delka zivota exploze
+func init(pos, color, amount, velocity, size, lifetime, time):
+	position.x = pos.x
+	position.y = pos.y
+	$Particles2D.emitting = true
+	$Timer.wait_time = time
+	$Timer_death.wait_time = int(1.5 * lifetime)
+	$Timer.start()
+	$Particles2D.amount = amount
+	$Particles2D.process_material.scale = size
+	$Particles2D.process_material.initial_velocity = velocity
+	$Particles2D.lifetime = lifetime
+	$Particles2D.process_material.color = color
+
+
+# zastaveni exploze + spusteni timeru na volani destruktoru
+func _on_Timer_timeout():
+	$Particles2D.emitting = false
+	$Timer_death.start()
+
+
+# zavola destruktor
+func _on_Timer_death_timeout():
+	queue_free()
